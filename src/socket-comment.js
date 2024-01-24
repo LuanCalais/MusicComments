985b56b6f3b7186613c047747a6b62d3335729f8
LuanCalais <luancalais@gmail.com>
2024-01-24 11:52:50 -0300

    create mock information to get in connection event

--- src/socket-comment.js
+++ src/socket-comment.js
@@ -1,11 +1,33 @@
 import io from "./server.js";
 
+const comments = [
+    {
+        name: "Indie",
+        value: "Value indie example mock"
+    },
+    {
+        name: "Rock",
+        value: "Value rock example mock"
+    },
+    {
+        name: "Pop",
+        value: "Value pop example mock"
+    },
+]
+
+function findComment(name) {
+    return comments.find(item => item.name === name)?.value 
+}
+
 // When some client emit a event called "connection" the server will listen and done something 
 io.on("connection", (socket) => {
     console.log('A user has connected')
 
     // this is activate when some enter in the selected gender 
     socket.on("gender_select", (name) => {
+        const value = findComment(name)
+        console.log(value)
+
         // join - Create a room
         // join - Get the client and group him in a room with the gender type name
         socket.join(name)
@@ -18,6 +40,16 @@ io.on("connection", (socket) => {
 
         // Set the information to the correct room and clients there
         socket.to(name).emit("text_action_client", text)
+
+        // Emit of the event include the client connected with the socket
+        // io.to(name).emit("text_action_client", text)
+
+        // Emit with a array of rooms 
+        // socket.to([name, 'Indie']).emit("text_action_client", text)
+
+        // Emit a event to the rooms with one exception 
+        // socket.to(["Indie", "Country"]).except("Rock").emit("text_action_client", text);
+
     })
 
     socket.on("disconnect", (reason) => {
