68f980c54ff613e008316de160cafea07ecab129
LuanCalais <luancalais@gmail.com>
2024-01-25 11:04:05 -0300

    init mongo db connection

---
+++ src/config/dbConnect.js
@@ -0,0 +1,20 @@
+import { MongoClient } from "mongodb";
+
+const client = new MongoClient("mongodb+srv://KingOfNothing:125678943@kingofnothing.huxpw63.mongodb.net/?retryWrites=true&w=majority")
+
+let commentsCollection
+
+try {
+    await client.connect()
+
+    const db = client.db("MusicComments")
+    commentsCollection = db.collection("comments")
+
+    console.log(comments)
+    console.log("Database connection is done")
+
+} catch (err) {
+    console.log(`Cannot connect to mongo db: ${err}`)
+}
+
+export { commentsCollection }
